408efd3e1ae45c8f2bcdebdbcca030c9299c21bb
eunho <eunho1032@gmail.com>
2025-11-16 17:23:56 +0900

    feat: 프로젝트 셋업

---
+++ src/test/java/calculator/ApplicationTest.java
@@ -0,0 +1,31 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ApplicationTest extends NsTest {
+    @Test
+    void custom_delimiter_test() {
+        assertSimpleTest(() -> {
+            run("//;\\n1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
+    void exception_test() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("-1,2,3"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
